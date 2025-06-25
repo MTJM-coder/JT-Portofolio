ecb0849fa13d76c0974d9823f224942531b165fe
MTJM <jaudelmerlando@gmail.com>
2025-06-25 21:55:42 +0100

    Ajout du composant Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // importer les animations
 import 'animate.css';
-import Navbar from './composant/navbar'
+import Navbar from './composant/Navbar'
 import Home from './composant/Home'
 import About from './composant/About'
 import Education from './composant/Education';
